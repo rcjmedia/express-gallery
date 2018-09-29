a82c1b59fa334bb0bc3f06198ee3c3dd3195fdd4
Romeo <romeocorpuz808@gmail.com>
2018-09-28 19:35:38 -1000

    Setup docker and got hello world working with handlebars
    Setup docker and got hello world working with handlebars

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-const PORT = 9090;
+const express = require('express');
+const app = express();
+const bp = require('body-parser');
+const exphbs = require('express-handlebars')
+const PORT = process.env.EXPRESS_CONTAINER_PORT || 9090;
 
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', '.hbs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  // use the knex variable above to create dynamic queries
+  res.render('index');
+});
 
 app.listen(PORT, () => {
-    console.log(`Listening on PORT: ${PORT}`)
+  console.log(`Listening on port: ${PORT}`);
 });
